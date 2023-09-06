552fe3819deb4b36bd40d292ec68c605c6cbc493
thoaria <accostan@buffalo.edu>
2023-09-05 21:21:04 -0400

    changes made </3

--- util/request.py
+++ util/request.py
@@ -2,7 +2,7 @@ class Request:
 
     def __init__(self, request: bytes):
         # TODO: parse the bytes of the request and populate the following instance variables
-
+        self.string = request.decode('utf-8')
         self.body = b""
         self.method = ""
         self.path = ""
